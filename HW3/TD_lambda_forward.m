35c9e87f62a6dea010ca0afb8cc0e8b97c1f6778
fridsamt <zhangyue2016@hotmail.com>
2017-03-17 14:54:24 -0400

    fix TD_lambda

--- HW3/TD_lambda_forward.m
+++ HW3/TD_lambda_forward.m
@@ -25,26 +25,27 @@ v_pi = initial_v_pi;
 [statesFromEpisodes, actionsFromEpisodes, rewardsFromEpisodes] = getEpisodes(stateSpace, policy, num_episodes);
 all_v_pi = zeros(size(stateSpace, 1), num_episodes);
 for i = 1 : num_episodes
-trace = zeros(size(stateSpace, 1), 1);
 states = cell2mat(statesFromEpisodes(i));
 rewards = cell2mat(rewardsFromEpisodes(i));
 length_states = size(states, 1);
-state_index = find( all( repmat(states(1, :), size(stateSpace, 1),1) == stateSpace, 2) );
-for j = 1 : length_states
-    trace(state_index) = trace(state_index) + 1;
-    if j ~= length_states
-        next_state_index = find( all( repmat(states(j + 1, :), size(stateSpace, 1),1) == stateSpace, 2) );
-        reward = rewards(j);
-    else
-        next_state_index = state_index;
-        reward = 0;
+for j = 1 : length_states - 1
+    state_index = find( all( repmat(states(j, :), size(stateSpace, 1),1) == stateSpace, 2) );
+    number_remain = length_states - j;
+    G_n = zeros(number_remain, 1);
+    for n = 1 : number_remain
+        last_index = find( all( repmat(states(j + n, :), size(stateSpace, 1),1) == stateSpace, 2) );
+        x = 0 : n - 1;
+        g = gamma.^x;
+        G_n(n) = dot(g , rewards(j:j + n - 1)) + (gamma^n)*v_pi(last_index);
     end
-    e = reward + gamma * v_pi(next_state_index) - v_pi(state_index);
-    for state = 1 : size(stateSpace, 1)
-        v_pi(state) = v_pi(state) + alpha * e * trace(state);
+    G_lambda = 0;
+    if number_remain ~= 1
+        x = 0 : number_remain - 2;
+        l = lambda.^x;
+        G_lambda = G_lambda + (1 - lambda)*dot(l , G_n(1:end - 1));
     end
-    state_index = next_state_index;
-    trace = trace * lambda * gamma;
+    G_lambda = G_lambda + lambda.^(number_remain - 1)*G_n(end);
+    v_pi(state_index) = v_pi(state_index) + alpha * (G_lambda - v_pi(state_index));
 end
 all_v_pi(:, i) = v_pi;
 end
